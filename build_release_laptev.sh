2ce5519c0c49daf9aea4f52c336a3206dbe36b23
lisapro <elisaveta.pro@gmail.com>
2019-03-22 11:09:02 +0100

    removed ersem env var

--- build_release_laptev.sh
+++ build_release_laptev.sh
@@ -3,9 +3,8 @@
 mkdir -p build
 rm -r build/*
 CWD=`pwd`
-cp ../SPBM_data/laptev-2/* build &&
+cp ../data_spbm_laptev/baseline-0.001/* build &&
 cd build && cmake $CWD/src -DFABM_BASE=$FABMDIR \
-                           -DFABM_ERSEM_BASE=$ERSEMDIR \
                            -DFABM_NIVA_BASE=$BROMDIR \
                            -DCMAKE_BUILD_TYPE=Release \
                            #-DERSEM_USE_IRON=ON \
