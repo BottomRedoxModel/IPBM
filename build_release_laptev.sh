39f8d4ed1abe688f535891a0182f2a65d6394e00
lisapro <elisaveta.pro@gmail.com>
2019-04-11 10:42:57 +0200

    changed baseline path in bash for solution

--- build_release_laptev.sh
+++ build_release_laptev.sh
@@ -3,7 +3,7 @@
 mkdir -p build
 rm -r build/*
 CWD=`pwd`
-cp ../data_spbm_laptev/baseline-0.001/* build &&
+cp ../data_spbm_laptev/baseline-B/* build &&
 cd build && cmake $CWD/src -DFABM_BASE=$FABMDIR \
                            -DFABM_NIVA_BASE=$BROMDIR \
                            -DCMAKE_BUILD_TYPE=Release \
