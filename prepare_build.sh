27efe70a9bda3782d9c8bf41247e8f7cafb5a4f5
shamil yakubov <limash@localhost.localdomain>
2020-03-29 23:58:46 +0200

    Add a new shell file

--- prepare_build.sh
+++ prepare_build.sh
@@ -3,16 +3,16 @@
 args=(-DFABM_BASE=$FABMDIR -DFABM_ERSEM_BASE=$ERSEMDIR -DFABM_NIVA_BASE=$BROMDIR -DCMAKE_BUILD_TYPE=Release)
 
 if [[ $# -ne 1 ]] ; then
-    echo 'Please specify arguments north or north-2'
-	exit 1
+    echo 'Please specify an argument: north or north-2'
+    exit 1
 fi
 
 if [[ ( "$1" = 'north' || "$1" = 'north-2' ) ]] ; then
-	mkdir -p build
-	rm -r build/* 2> /dev/null
-	CWD=$(pwd)
-	cp ../SPBM_data/$1/* build && cd build && cmake $CWD/src "${args[@]}"
+    mkdir -p build
+    rm -r build/* 2> /dev/null
+    CWD=$(pwd)
+    cp ../SPBM_data/$1/* build && cd build && cmake $CWD/src "${args[@]}"
 else
-	echo 'Wrong arguments'
-	exit 1
+    echo 'Wrong arguments'
+    exit 1
 fi
